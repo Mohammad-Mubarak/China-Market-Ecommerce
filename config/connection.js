a969b300da5a12e8a439aa5c7fe115166d9f41e0
Mohammad-Mubarak <beyourbestversion3@gmail.com>
2023-03-26 04:05:02 +0530

    updated

--- config/connection.js
+++ config/connection.js
@@ -24,7 +24,13 @@ require("dotenv").config();
 
 const mongo=require("mongoose")
 const mongo_url=process.env.MONGO_DB_URL
+
+// const mongo_url="mongodb+srv://mubarak:8800732325@cluster0.thukbqk.mongodb.net/chinamarket?retryWrites=true&w=majority"
+
 // const mongo_url="mongodb+srv://md:8800@cluster0.hzxrinb.mongodb.net/?retryWrites=true&w=majority"
+
+
+
 
 
 // connecting to mongo db
