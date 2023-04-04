83dd912e2ba62de78b0172ab267e7a6c18357efc
Mohammad Mubarak <115584175+Mohammad-Mubarak@users.noreply.github.com>
2023-04-04 05:50:16 +0530

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,42 +1,11 @@
-// const mongoose = require('mongoose');
-
-//  require("dotenv").config()
-
-// const connectDB = ()=>{ 
-// mongoose.connect(process.env.MONGO_DB_URL, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-// }).then(response=>{
-//     console.log('MongoDB Connection Succeeded.')
-// }).catch(error=>{
-//     console.log('Error in DB connection: ' + error)
-// });
-// }
-
-
-// module.exports = connectDB
-
-
-
-
 
 require("dotenv").config();
 
 const mongo=require("mongoose")
 const mongo_url=process.env.MONGO_DB_URL
 
-// const mongo_url="mongodb+srv://mubarak:8800732325@cluster0.thukbqk.mongodb.net/chinamarket?retryWrites=true&w=majority"
-
-// const mongo_url="mongodb+srv://md:8800@cluster0.hzxrinb.mongodb.net/?retryWrites=true&w=majority"
-
-
-
-
-
 // connecting to mongo db
    mongo.set('strictQuery', false)
-
-
     mongo.connect(mongo_url,{
         useNewUrlParser:true,
         useUnifiedTopology:true
